a8ded9f16bedd124c9b4b4ee5fff8b57114eea6f
capyfara <115417692+capyfara@users.noreply.github.com>
2026-05-19 15:37:56 +0800

    Add: duplicate-integer - submission-6

---
+++ Data Structures & Algorithms/duplicate-integer/submission-6.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums) {
+            if (set.contains(n) == false) {
+                set.add(n);
+            } else return true;
+        }
+        return false;
+    }
+}
