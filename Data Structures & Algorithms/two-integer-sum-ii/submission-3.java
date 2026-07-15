2a04a3347eed634f2552236be52f2867febcc556
capyfara <115417692+capyfara@users.noreply.github.com>
2026-07-15 12:49:22 +0800

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        // How do i use the 2 pointer method here
+        // Oh, I just base it on the sum
+        int l = 0;
+        int r = numbers.length - 1;
+        while (l < r) {
+            int sum = numbers[l] + numbers[r];
+            if (sum == target) {
+                return new int[] {l + 1, r + 1};
+            } else if (sum < target) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return new int[0];
+    }
+}
